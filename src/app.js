5c810cf08ca6f13b4d13e8b651c6ca4829889e9c
shaunmaselela25 <shaunmaselela25@icloud.com>
2026-02-25 12:57:21 +0000

    Implemented logs and logging

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import logger from '#config/logger';
+import helmet from "helmet";
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  logger.info('Hello from acquisitions API!');
   res.status(200).send('Welcome to acquisitions API!');
 });
 
